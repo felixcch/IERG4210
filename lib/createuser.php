b4bda3d8b3006b363faa216f93d3e81c422cf482
felixcch <32195330+felixcch@users.noreply.github.com>
2017-11-12 22:36:43 +0800

    Add files via upload

--- lib/createuser.php
+++ lib/createuser.php
@@ -5,27 +5,37 @@
  * Date: 11/11/2017
  * Time: 14:54
  */
+
 include ("db.inc.php");
-$salt = mt_rand();
-echo $salt;
-$saltedpassword = hash_hmac('sha256',"badguy74103665",$salt);
-echo $saltedpassword;
 $db = ierg4210_DB();
+$q = $db->prepare("DROP TABLE user");
+$q->execute();
 $q = $db->prepare("CREATE TABLE user ( userid INT PRIMARY KEY, email TEXT, salt TEXT, password TEXT, isAdmin BOOLEAN)");
 $q->execute();
-$q = $db->prepare("INSERT INTO user VALUES (1,'admin@gmail.com',(?),(?),0 )");
+$salt = uniqid(mt_rand(), true);
+$options = [
+    'salt' => $salt, //write your own code to generate a suitable salt
+    'cost' => 12 // the default cost is 10
+];
+$saltedpassword = password_hash('badguy74103665', PASSWORD_DEFAULT, $options);
+
+$q = $db->prepare("INSERT INTO user VALUES (1,'admin@gmail.com',(?),(?),1 )");
 $result = $q->execute(array($salt,$saltedpassword));
-$salt = mt_rand();
-echo $salt;
-$saltedpassword = hash_hmac('sha256',"test123",$salt);
-echo $saltedpassword;
+$salt = uniqid(mt_rand(), true);
+$options = [
+    'salt' => $salt, //write your own code to generate a suitable salt
+    'cost' => 12 // the default cost is 10
+];
+$saltedpassword = password_hash('test123', PASSWORD_DEFAULT, $options);
 $q = $db->prepare("INSERT INTO user VALUES (2,'user1@gmail.com',(?),(?),0 )");
 $result = $q->execute(array($salt,$saltedpassword));
-$salt = mt_rand();
-echo $salt;
-$saltedpassword = hash_hmac('sha256',"test123",$salt);
-echo $saltedpassword;
-$q = $db->prepare("INSERT INTO user VALUES (3,'user12@gmail.com',(?),(?),0 )");
+$salt = uniqid(mt_rand(), true);
+$options = [
+    'salt' =>$salt, //write your own code to generate a suitable salt
+    'cost' => 12 // the default cost is 10
+];
+$saltedpassword = password_hash('test123', PASSWORD_DEFAULT, $options);
+$q = $db->prepare("INSERT INTO user VALUES (3,'user2@gmail.com',(?),(?),0 )");
 $result = $q->execute(array($salt,$saltedpassword));
 echo "Success";
 ?>
